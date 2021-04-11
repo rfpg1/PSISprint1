c587487c751a8ff593d42f0b20b475ddb656f9dc
fc52765 <fc52765@alunos.fc.ul.pt>
2021-04-11 19:06:41 +0100

    controllers

---
+++ controllers/userController.js
@@ -0,0 +1,12 @@
+var User = require('../models/user');
+
+var mongoose = require('mongoose');
+
+exports.post_user = function (req, res, next) {
+    var new_user = new User(req.body);
+    
+    new_user.save(function (err, user) {
+        if (err) { res.send(err); }
+        res.json(user);
+    })
+}

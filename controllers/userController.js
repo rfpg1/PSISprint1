a1bdcea803853888a368afac55670c164d5d007e
fc52765 <fc52765@alunos.fc.ul.pt>
2021-04-11 19:41:57 +0100

    DBInit

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,9 +4,6 @@ var mongoose = require('mongoose');
 
 exports.post_user = function (req, res, next) {
     var new_user = new User(req.body);
+    res.json(req.body);
     
-    new_user.save(function (err, user) {
-        if (err) { res.send(err); }
-        res.json(user);
-    })
 }

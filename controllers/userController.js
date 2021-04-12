1c30f71d30138f76596526aa0c429c186367b58a
fc52765 <fc52765@alunos.fc.ul.pt>
2021-04-12 12:05:45 +0100

    registos duplos

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,9 +3,17 @@ var User = require('../models/user');
 var mongoose = require('mongoose');
 
 exports.post_user = function (req, res, next) {
-    var new_user = new User(req.body);
-    new_user.save(function (err, user) {
-        if (err) { res.send(err); }
-        res.json(user);
-    })
+    var u = null;
+    User.find({ name: req.body.name, pw: req.body.pw })
+        .exec(function (err, user) {
+            if (err) { return next(err) }
+            u = user;
+        })
+    if(u === null){
+        var new_user = new User(req.body);
+        new_user.save(function (err, user) {
+            if (err) { res.send(err); }
+            res.json(user);
+        })
+    }
 }

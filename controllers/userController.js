8ae7a4688385e8ab8954f0d133a2e824aa131993
fc52765 <fc52765@alunos.fc.ul.pt>
2021-04-12 12:37:17 +0100

    login done

--- controllers/userController.js
+++ controllers/userController.js
@@ -11,9 +11,17 @@ exports.get_user = function (req, res, next) {
 }
 
 exports.post_user = function (req, res, next) {
-    var new_user = new User(req.body);
-    new_user.save(function (err, user) {
-        if (err) { res.send(err); }
-        res.json(user);
-    })
+    User.find({ name: req.body.name})
+        .exec(function (err, user) {
+            if (err) { return next(err) }
+            if(user.length === 0){
+                var new_user = new User(req.body);
+                new_user.save(function (err, user) {
+                    if (err) { res.send(err); }
+                    res.json(user);
+                })
+            } else {
+                res.json({message:"User already exists"});
+            }
+        })
 }

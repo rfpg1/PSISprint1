c6260878a200ad189af4fd48de2b8d6bb8172d67
Daniel Batista <fc52773@alunos.fc.ul.pt>
2021-04-12 18:23:36 +0100

    sim

--- controllers/photosController.js
+++ controllers/photosController.js
@@ -1,7 +1,7 @@
 var Photo = require('../models/photo');
 
 exports.post_photo = function (req, res, next) {
-    //var Cr7 = new Photo(req.body);
+    var Cr7 = new Photo(req.body);
     //Cr7.save(function (err, photo) {
     //    if (err) { return next(err) }
     //    res.json( photo );

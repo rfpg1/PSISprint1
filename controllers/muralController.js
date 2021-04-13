3ca87786d6823e10b8c2d7d99cf075ee7a1c8491
Rafael Abrantes <fc52751@alunos.fc.ul.pt>
2021-04-13 15:02:58 +0100

    muralPage3

--- controllers/muralController.js
+++ controllers/muralController.js
@@ -4,7 +4,6 @@ var mongoose = require('mongoose');
 
 exports.get_recent = function (req, res, next) {
     Photo.find()
-        .sort([['date', '(a, b) => b.date - a.date)']])
         .slice(0, 50)
         .exec(function (err, list_photo){
             if (err) { return next(err); }

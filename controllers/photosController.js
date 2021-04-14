aedbc09368155c7e97ba5145bef9cf2a835c3ea0
Daniel Batista <fc52773@alunos.fc.ul.pt>
2021-04-14 14:32:10 +0100

    del photo

--- controllers/photosController.js
+++ controllers/photosController.js
@@ -12,6 +12,6 @@ exports.delete_photo = function (req, res, next) {
     var id = req.params.id;
     Hero.findByIdAndDelete(id, function (err, photo) {
         if (err) { return next(err) }
-        res.json({ message: 'Photo deleted successfully' })
+        res.json({ message: 'Photo deleted successfully ' })
     })
 };

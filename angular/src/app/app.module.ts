f280bff0ea56905c0f1a05e0d8c0f6fc3bcb8ee2
Goncalo Nogueira <fc52759@alunos.fc.ul.pt>
2021-04-15 15:32:26 +0100

    Bazooka3

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { LoginComponent } from './login/login.component';
 import { RegistarComponent } from './registar/registar.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { ReactiveFormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]

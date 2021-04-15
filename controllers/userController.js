2e6774ff23cfa46c4153fbf680efb612e5258cfc
fc52765 <fc52765@alunos.fc.ul.pt>
2021-04-15 22:14:33 +0100

    gitignore

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,6 +3,7 @@ var User = require('../models/user');
 var mongoose = require('mongoose');
 
 exports.get_user = function (req, res, next) {
+    console.log(req.params)
     console.log(req.params.name)
     console.log(req.params.pw)
     User.find({ name: req.params.name, pw: req.params.pw })

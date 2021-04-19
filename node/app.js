95357b55574eed61a53b370c5af90f9c9bad1f24
fc52765 <fc52765@alunos.fc.ul.pt>
2021-04-19 14:01:23 +0100

    catch error no add

--- node/app.js
+++ node/app.js
@@ -22,13 +22,14 @@ var cors = require('cors')
     }));
     */
 var app = express();
+/*
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
-
-//app.use(cors())
+*/
+app.use(cors())
 
 //Set up mongoose connection
 var mongoose = require('mongoose');

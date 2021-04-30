92ba105f9bd94154f9664f93f745335a1b26110d
fc52765 <fc52765@alunos.fc.ul.pt>
2021-04-30 19:16:37 +0100

    changed default page

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { RegistarComponent } from './registar/registar.component';
 import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/registar', pathMatch: 'full'},
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
   { path: 'dashboard', component: WallComponent},
   { path: 'login', component: LoginComponent },
   { path: 'registar', component: RegistarComponent },

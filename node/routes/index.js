26c855f8aaa616722319a4b8bed7b35ea923e797
fc52765 <fc52765@alunos.fc.ul.pt>
2021-05-02 00:35:36 +0100

    init fixed

--- node/routes/index.js
+++ node/routes/index.js
@@ -6,51 +6,53 @@ var Photo = require('../models/photo');
 var async = require('async');
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.json('teste');
+router.get('/', function(req, res, next) {
+    res.json('teste');
 });
 
 var users = [];
 
 router.get('/init', (req, res, next) => {
 
-  async.series(
-    deleteDB(),
-    userCreate('Richie', "123"),
-    userCreate('Daniel', "123")
-  )
-  res.json('DB created');
+    async.series(
+        deleteDB(),
+        userCreate('Richie', "123"),
+        userCreate('Daniel', "123")
+    )
+    res.json('DB created');
 })
 
 function deleteDB() {
-  User.collection.drop();
-  Photo.collection.drop();
+    User.collection.drop();
+    Photo.collection.drop();
+    Favorite.collection.drop();
+    Like.collection.drop();
 }
 
 //Very startings
 function userCreate(name, pw) {
-  userDetail = { name: name, pw: pw }
-  var user = new User(userDetail);
-  users.push(user);
-  user.save(function (err) {
-    if (err) {
-      console.log('User could not be created during dbinit')
-      return;
-    }
-    console.log('New user: ' + name);
-  });
+    userDetail = { name: name, pw: pw }
+    var user = new User(userDetail);
+    users.push(user);
+    user.save(function(err) {
+        if (err) {
+            console.log('User could not be created during dbinit')
+            return;
+        }
+        console.log('New user: ' + name);
+    });
 }
 
 function photoCreate(user, photo, likes, date) {
-  photoDetail = { user: user, photo: photo, likes: likes, date: date }
-  var photo = new Photo(photoDetail);
-  photo.save(function (err) {
-    if (err) {
-      console.log('Photo could not be created during dbinit')
-      return
-    }
-    console.log('New photo added');
-  });
+    photoDetail = { user: user, photo: photo, likes: likes, date: date }
+    var photo = new Photo(photoDetail);
+    photo.save(function(err) {
+        if (err) {
+            console.log('Photo could not be created during dbinit')
+            return
+        }
+        console.log('New photo added');
+    });
 }
 //Very endings
 

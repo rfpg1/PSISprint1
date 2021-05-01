d51423f43ec3b7cb933f1ba61bbbb6120b4bd715
fc52765 <fc52765@alunos.fc.ul.pt>
2021-05-02 00:37:34 +0100

    init fixed

--- node/routes/index.js
+++ node/routes/index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var User = require('../models/user');
 var Photo = require('../models/photo');
+var Favorite = require('../models/favorite');
+var Like = require('../models/like');
 
 var async = require('async');
 
